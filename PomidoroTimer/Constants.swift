da4f24b9d5dab94a58a8f96495cec0fe08a36161
David Tonoyan <davidtonoyan10@me.com>
2022-12-23 14:46:42 +0400

    Add Constants file

---
+++ PomidoroTimer/Constants.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  PomidoroTimer
+//
+//  Created by Давид Тоноян  on 23.12.2022.
+//
+
+import UIKit
+
+enum Constants {
+    enum Color {
+        static let red = UIColor.red
+        static let white = UIColor.white
+    }
+    
+    enum Offset {
+        static let mediumOffset: CGFloat = 20
+    }
+    
+    enum FontSize {
+        static let largeSize: CGFloat = 80
+    }
+    
+    enum Size {
+        static let buttonSize: CGFloat = 40
+    }
+    
+    enum Image {
+        static let playImage = UIImage(systemName: "play")
+        static let playFillImage = UIImage(systemName: "play.fill")
+        
+        static let pauseImage = UIImage(systemName: "pause")
+        static let pauseFillImage = UIImage(systemName: "pause.fill")
+    }
+}
